907bc143993239fff2fde006ce6113ef20ceb45a
FabrJG2003 <fabricio.julian2507@gmail.com>
2025-10-01 20:20:31 -0500

    Primeras migraciones

---
+++ backend/database/migrations/2025_10_02_010913_create_usuarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('rol', ['administrador', 'colaborador']);
+            $table->string('id_sucursal')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
